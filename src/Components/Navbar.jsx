c2ab0e6efd78f5b425a8de98ebff7260aae00017
Anushree <anushreesaha21@outlook.com>
2024-04-02 00:35:00 +0530

    remove extra

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -39,32 +39,8 @@ const Navbar =()=> {
                 <li className="nav-item">
                   <Link className="nav-link" to="/technology">Technology</Link>
                 </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/business">Business</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">General</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/health">Health</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/science">Science</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sport">Sport</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/technology">Technology</Link>
-                </li>
-
               </ul>
-              </ul>
-
-            </div>
+              </div>
           </div>
         </nav>
       </div>
